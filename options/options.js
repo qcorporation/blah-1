d524abe72ea2f96b28ae4d35a3e1499044904202
qcorporation <quan.nguyen@crowdbotics.com>
2023-02-16 20:42:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blah-1",
+  url: "https://blah-1.botics.co",
+  api: "https://blah-1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
